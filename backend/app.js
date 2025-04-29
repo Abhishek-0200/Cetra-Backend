dd05f6f149bc6348452f08e7607b0e1c2e7007a4
Abhishek Sharma <abhisheksharma444412@gmail.com>
2025-04-29 15:42:56 +0530

    Added functionality for Handling the Payments

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import authRoutes from './routes/auth.route.js';
 import productRoutes from './routes/product.route.js';
 import connnetMongoDb from './db/connectToDb.js';
+import paymentRoutes from './routes/payment.route.js';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
@@ -9,11 +10,13 @@ import cookieParser from 'cookie-parser';
 dotenv.config();
 const app = express();
 
+app.use(cookieParser())
 // Middleware to parse JSON
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/payment", paymentRoutes);
 
 
 
